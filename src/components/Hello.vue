2056678832f93db83474910d60f97b1b18495954
xuzhenwu <xuzhenwu@tenmind.com>
2018-11-16 15:31:53 +0800

    da

---
+++ src/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="hello">
+		<h1>{{ hello }}</h1>
+	</div>
+</template>
+<script>
+	export default {
+		name: "Hello",
+		data() {
+	    return {
+	      hello: 'afsdfasdfasfas'
+	    }
+	  }
+	}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Hello from '@/components/Hello'
+
+Vue.use(Router)
+export default new Router({
+  base: '/vue/',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+      children: [
+      	{
+      		path:'/hello',
+      		name: "Hello",
+      		component: Hello
+      	}
+      ]
+    }
+  ]
+})
